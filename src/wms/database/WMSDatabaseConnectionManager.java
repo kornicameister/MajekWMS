9580bf1fae7203b5ba544848d0f96bbd3cb82bcc
kornicameister <kornicameister@gmail.com>
2012-09-18 02:43:02 +0200

    #7 application connects and disconnets in valid way

--- src/wms/database/WMSDatabaseConnectionManager.java
+++ src/wms/database/WMSDatabaseConnectionManager.java
@@ -1,90 +1,75 @@
 package wms.database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.sql.DataSource;
+
 /**
  * Class allows to either open or kill connection to specific database engine.
+ * 
  * @author kornicameister
- *
+ * 
  */
 public class WMSDatabaseConnectionManager {
-	private static WMSConnectionData credentials = null;
-	private static Connection connection = null;
-	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private Connection connection = null;
+	private final DataSource dataSource;
 	private static Logger log = Logger.getLogger(WMSDatabaseConnectionManager.class.getName());
 	
-	/**
-	 * Use this method to open the connection. 
-	 * <p style="color: red"> {@link WMSDatabaseConnectionManager.credentials} must be initialized before
-	 * calling this method</p>
-	 * 
-	 * @throws Exception if credentials had not been initialized 
-	 */
-	public static void openConnection() throws Exception {
-		if(WMSDatabaseConnectionManager.credentials == null){
-			throw new Exception("Unitialized credentials, connection wont open");
-		}
-		
+	public WMSDatabaseConnectionManager(DataSource dataSource) {
 		try {
-			log.log(Level.CONFIG, "Loading {0} database driver.",WMSDatabaseConnectionManager.DRIVER);
-			Class.forName(DRIVER).newInstance();
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e1) {
-			log.log(Level.SEVERE, "Failed to load com.mysql.jdbc.Driver driver");
-			e1.printStackTrace();
-		}
-
-		String url = "jdbc:mysql://!:!/!";
-
-		url = url.replaceFirst("!", credentials.getHost());
-		url = url.replaceFirst("!", credentials.getPort().toString());
-		url = url.replaceFirst("!", credentials.getDatabaseName());
-
-		try {
-			WMSDatabaseConnectionManager.connection = DriverManager.getConnection(url, credentials.getUserName(),credentials.getUserPassword());
-			log.logp(Level.INFO, "Connected to {0} engine using {1} user name", WMSDatabaseConnectionManager.DRIVER, credentials.getUserName());
-		} catch (SQLException e) {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+		this.dataSource = dataSource;
 	}
-	
+
 	/**
-	 * Encapsulates safe way to currently alive connection established to {@link WMSDatabaseConnectionManager.DRIVER} engine.
+	 * Use this method to open the connection.
+	 * <p style="color: red">
+	 * {@link WMSDatabaseConnectionManager.credentials} must be initialized
+	 * before calling this method
+	 * </p>
+	 * 
 	 */
-	public static void killConnection(){
-		if (WMSDatabaseConnectionManager.connection != null) {
+	public void openConnection() {
+		try {
+			this.connection = this.dataSource.getConnection();
+			log.log(Level.INFO, "Connected to database {0}", this.connection.getCatalog());
+		} catch (SQLException e) {
+			log.log(Level.SEVERE, "Failed to connect to database", e);
+		}
+	}
+
+	/**
+	 * Encapsulates safe way to currently alive connection established to
+	 * {@link WMSDatabaseConnectionManager.DRIVER} engine.
+	 */
+	public void killConnection() {
+		if (this.connection != null) {
 			try {
-				WMSDatabaseConnectionManager.connection.close();
-				if (WMSDatabaseConnectionManager.connection.isClosed()) {
+				this.connection.close();
+				if (this.connection.isClosed()) {
 					log.log(Level.INFO, "Connection terminated...");
 				}
-				WMSDatabaseConnectionManager.connection = null;
+				this.connection = null;
 			} catch (SQLException e) {
-				log.log(Level.WARNING, "Connection couldn't be terminated and it still active...");
-				e.printStackTrace();
+				log.log(Level.WARNING, "Connection couldn't be terminated and it still active...", e);
 			}
 		}
 	}
 
-	public static WMSConnectionData getCredentials() {
-		return WMSDatabaseConnectionManager.credentials;
-	}
-
-	public static void setCredentials(WMSConnectionData credentials) {
-		WMSDatabaseConnectionManager.credentials = credentials;
-	}
-
-	public static Connection getConnection() throws Exception {
-		if(WMSDatabaseConnectionManager.connection == null){
-			WMSDatabaseConnectionManager.openConnection();
+	public Connection getConnection() throws Exception {
+		if (this.connection == null) {
+			this.openConnection();
 		}
-		return WMSDatabaseConnectionManager.connection;
+		return this.connection;
 	}
 
-	public static void setConnection(Connection connection) {
-		WMSDatabaseConnectionManager.connection = connection;
+	public void setConnection(Connection connection) {
+		this.connection = connection;
 	}
 }
--- src/wms/listeners/WMSDatabaseBridgeInitializer.java
+++ src/wms/listeners/WMSDatabaseBridgeInitializer.java
@@ -1,36 +1,84 @@
 package wms.listeners;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.Resource;
+import javax.annotation.Resource.AuthenticationType;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+
+import wms.database.WMSDatabaseConnectionManager;
 
 /**
  * Application Lifecycle Listener implementation class WMSDatabaseBridge
- *
+ * 
  */
 @WebListener
 public class WMSDatabaseBridgeInitializer implements ServletContextListener {
+	private static Logger log = Logger.getLogger(WMSDatabaseBridgeInitializer.class.getName());
+	private WMSDatabaseConnectionManager connectionManager;
+
+	@Resource(
+			name = "jdbc/majekwms",
+			description = "MySQL resources, data bridge",
+			authenticationType = AuthenticationType.CONTAINER,
+			shareable = true,
+			type = javax.sql.DataSource.class,
+			lookup = "jdbc/majekwms"
+	)
+	private DataSource majekWMSDataSource;
+	
 	/**
 	 * Upon this method calling following operations are performed:
+	 * 
 	 * <pre>
 	 * <ol>
 	 * <li>Connection to database is established</li>
 	 * <li>Established credentials are saved to allow further access</li>
 	 * </ol>
 	 * </pre>
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent arg0) {
-        // TODO Auto-gen
-    }
+	 * 
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent context) {
+		// initializing resources.
+		this.connectionManager = new WMSDatabaseConnectionManager(this.majekWMSDataSource);
+		
+		// and loading them.
+		WMSDatabaseBridgeInitializer.log.entering(WMSDatabaseBridgeInitializer.class.getSimpleName(), "contextInitialized");
+		this.connectionManager.openConnection();
+		this.connectionManager.killConnection();
+		WMSDatabaseBridgeInitializer.log.exiting(WMSDatabaseBridgeInitializer.class.getSimpleName(), "contextInitialized");
+	}
 
 	/**
-	 * When this method is called server must terminate existing database connection.
+	 * When this method is called server must terminate existing database
+	 * connection.
+	 * Id does following:
+	 * <ul>
+	 * <li>unregister all registered drivers</li>
+	 * </ul>
 	 * 
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent arg0) {
-        // TODO Auto-generated method stub
-    }
-	
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent context) {
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            try {
+                DriverManager.deregisterDriver(driver);
+                log.log(Level.INFO, String.format("deregistering jdbc driver: %s", driver));
+            } catch (SQLException e) {
+                log.log(Level.SEVERE, String.format("Error deregistering driver %s", driver), e);
+            }
+        }
+	}
+
 }
