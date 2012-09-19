ae6c7707e47a943b1b0db71b678ad1917582614c
kornicameister <kornicameister@gmail.com>
2012-09-19 23:36:18 +0200

    #10 updated data model in mysql with additional fields

--- src/wms/model/Warehouse.java
+++ src/wms/model/Warehouse.java
@@ -1,5 +1,27 @@
 package wms.model;
 
-public class Warehouse {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "warehouse")
+public class Warehouse {
+	private Long id;
+	
+	@Id
+	@Column(name = "idWarehouse")
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
