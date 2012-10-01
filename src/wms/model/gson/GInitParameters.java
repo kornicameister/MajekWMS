88b4c726a361ae7adfd4e2cf10298f991d3c94b9
kornicameister <kornicameister@gmail.com>
2012-10-01 14:01:02 +0200

    #8 server is able to respond in json format

--- src/wms/model/gson/GInitParameters.java
+++ src/wms/model/gson/GInitParameters.java
@@ -1,53 +1,80 @@
 package wms.model.gson;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 
+import wms.model.hibernate.UnitType;
 import wms.model.hibernate.Warehouse;
 
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
-import com.google.gson.annotations.Since;
 
 public class GInitParameters {
-	@Since(0.1)
-	private HashSet<Warehouse> warehouses;
-
-	@Since(0.1)
-	@SerializedName("_dc")
-	private String getID;
+	private Configuration configuration;
 
 	public GInitParameters() {
 		super();
-		this.warehouses = new HashSet<>(0);
+		this.configuration = new Configuration();
 	}
 
-	public final HashSet<Warehouse> getWarehouses() {
-		return warehouses;
+	public GInitParameters(Configuration cfg) {
+		super();
+		this.configuration = cfg;
 	}
 
-	public final String getGetID() {
-		return getID;
+	public Configuration getConfiguration() {
+		return configuration;
 	}
 
-	public void setWarehouses(final HashSet<Warehouse> warehouses) {
-		this.warehouses = warehouses;
-	}
-
-	public void setWarehouses(List<Warehouse> readWarehouse) {
-		HashSet<Warehouse> tmp = new HashSet<>(
-				((ArrayList<Warehouse>) readWarehouse));
-		if (tmp != null) {
-			this.warehouses = tmp;
-		}
-	}
-
-	public void setGETId(String string) {
-		this.getID = string;
+	public void setConfiguration(Configuration configuration) {
+		this.configuration = configuration;
 	}
 
 	public String toGsonString() {
 		return new Gson().toJson(this, GInitParameters.class);
+	}
+
+	public class Configuration {
+		HashSet<Warehouse> warehouses;
+		HashSet<UnitType> unitTypes;
+
+		@SerializedName("_dc")
+		String getID;
+
+		public Configuration() {
+			super();
+		}
+
+		public Configuration(HashSet<Warehouse> warehouses,
+				HashSet<UnitType> unitTypes, String getID) {
+			super();
+			this.warehouses = warehouses;
+			this.unitTypes = unitTypes;
+			this.getID = getID;
+		}
+
+		public final HashSet<Warehouse> getWarehouses() {
+			return warehouses;
+		}
+
+		public final HashSet<UnitType> getUnitTypes() {
+			return unitTypes;
+		}
+
+		public final String getGetID() {
+			return getID;
+		}
+
+		public final void setWarehouses(HashSet<Warehouse> warehouses) {
+			this.warehouses = warehouses;
+		}
+
+		public final void setUnitTypes(HashSet<UnitType> unitTypes) {
+			this.unitTypes = unitTypes;
+		}
+
+		public final void setGetID(String getID) {
+			this.getID = getID;
+		}
+
 	}
 }
--- src/wms/servlets/WMSInitAgent.java
+++ src/wms/servlets/WMSInitAgent.java
@@ -1,7 +1,12 @@
 package wms.servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -21,7 +26,11 @@ import org.hibernate.service.ServiceRegistryBuilder;
 
 import wms.model.WMSModelConstants;
 import wms.model.gson.GInitParameters;
+import wms.model.hibernate.UnitType;
 import wms.model.hibernate.Warehouse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * {@link WMSInitAgent} acts as a middle-man between server and client in
@@ -30,7 +39,7 @@ import wms.model.hibernate.Warehouse;
  * This particular servlet must connect itself to database, select all required
  * information, transform them into valid JSON form and return to the client.
  */
-@WebServlet(urlPatterns = { "/wms/start" }, asyncSupported = true, loadOnStartup = 1, name = "WMSInitAgent")
+@WebServlet(urlPatterns = { "/wms/start/*" }, asyncSupported = true, loadOnStartup = 1, name = "WMSInitAgent")
 public class WMSInitAgent extends HttpServlet implements WMSServerClientAgent {
 	private static final long serialVersionUID = -217845239414591742L;
 	private static Logger logger = Logger.getLogger(WMSInitAgent.class
@@ -48,27 +57,136 @@ public class WMSInitAgent extends HttpServlet implements WMSServerClientAgent {
 		this.closeHibernateConnection();
 	}
 
+	/**
+	 * Utility method that allows to obtain CRUD action, as the last part of the
+	 * request's URI.
+	 * 
+	 * @param request
+	 * @return a CRUD action description
+	 */
+	private String getCRUDAction(HttpServletRequest request) {
+		String uri[] = request.getRequestURI().split("/");
+		String action = uri[uri.length - 1];
+		return action;
+	}
+
+	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		Map<String, String[]> params = request.getParameterMap();
-
-		GInitParameters init = this.loadRequired(params.get("requires"));
-		init.setGETId(params.get("_dc")[0]);
-
+		String action = getCRUDAction(request);
 		PrintWriter out = response.getWriter();
-		out.write(init.toGsonString());
+
+		logger.info(String.format("Request [ %s ]  detected, processing...",
+				action));
+		if (action.equals("read")) {
+			Map<String, String[]> params = request.getParameterMap();
+
+			GInitParameters init = this.loadRequired(params.get("requires"));
+			init.getConfiguration().setGetID(params.get("_dc")[0]);
+			out.write(init.toGsonString());
+		} else {
+			logger.warning(String.format(
+					"Unknown request GET[ %s ] for the servlet", action));
+			out.write("{success: false, message: 'Unknow request " + action
+					+ "'}");
+		}
 	}
-	
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String action = getCRUDAction(req);
+		PrintWriter out = resp.getWriter();
+
+		logger.info(String.format("Request [ %s ]  detected, processing...",
+				action));
+		if (action.equals("update")) {
+			StringBuilder stringBuilder = new StringBuilder();
+			BufferedReader bufferedReader = null;
+			try {
+				InputStream inputStream = req.getInputStream();
+				if (inputStream != null) {
+					bufferedReader = new BufferedReader(new InputStreamReader(
+							inputStream));
+					char[] charBuffer = new char[128];
+					int bytesRead = -1;
+					while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+						stringBuilder.append(charBuffer, 0, bytesRead);
+					}
+				} else {
+					stringBuilder.append("");
+				}
+			} catch (IOException ex) {
+				throw ex;
+			} finally {
+				if (bufferedReader != null) {
+					try {
+						bufferedReader.close();
+					} catch (IOException ex) {
+						throw ex;
+					}
+				}
+			}
+			Gson gson = new GsonBuilder().setDateFormat("m/d/y").create();
+			Warehouse newWarehouse = (Warehouse) gson
+					.fromJson(stringBuilder.toString(), GInitParameters.class)
+					.getConfiguration().getWarehouses().toArray()[0];
+			Serializable newWarehouseId = null;
+
+			Session session = this.sessionFactory.openSession();
+			session.beginTransaction();
+			try {
+				if (newWarehouse != null) {
+					newWarehouseId = session.save(newWarehouse);
+				}
+				session.getTransaction().commit();
+			} catch (Exception e) {
+				logger.log(Level.SEVERE,
+						String.format("Error when inserting warehouse..."), e);
+				session.getTransaction().rollback();
+				out.write("{success: failure, message: " + e.getMessage() + "}");
+			}
+
+			out.write("{success: true, warehouseId: " + newWarehouseId + "}");
+		} else {
+			logger.warning(String.format(
+					"Unknown request PUT[ %s ] for the servlet", action));
+			out.write("{success: false, message: 'Unknow request " + action
+					+ "'}");
+		}
+	}
+
 	private GInitParameters loadRequired(String[] param) {
 		GInitParameters initParameters = new GInitParameters();
 		for (String p : param[0].split(",")) {
-			if(p.equals("warehouse")){
-				initParameters.setWarehouses(this.readWarehouse());
+			if (p.equals("warehouses")) {
+				initParameters.getConfiguration().setWarehouses(
+						new HashSet<Warehouse>(this.readWarehouse()));
+			} else if (p.equals("unitTypes")) {
+				initParameters.getConfiguration().setUnitTypes(
+						new HashSet<UnitType>(this.readUnitTypes()));
+			} else {
+				logger.warning(String.format(
+						"Unknow required property key [ %s ]", p));
+				return initParameters;
 			}
+			logger.info(String.format(
+					"Request for [ %s ], database query finished with success",
+					p));
 		}
-		
+
 		logger.log(Level.INFO, "Required properties count = {0}", param.length);
 		return initParameters;
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<UnitType> readUnitTypes() {
+		Session session = this.sessionFactory.openSession();
+		session.beginTransaction();
+		List<UnitType> result = session.createQuery("from UnitType").list();
+		session.getTransaction().commit();
+
+		return result;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -77,12 +195,8 @@ public class WMSInitAgent extends HttpServlet implements WMSServerClientAgent {
 		session.beginTransaction();
 		List<Warehouse> result = session.createQuery("from Warehouse").list();
 		session.getTransaction().commit();
-		
-		return result;
-	}
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+		return result;
 	}
 
 	@Override
