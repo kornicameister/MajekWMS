f0a413119dd48f2991f172f7e1c57d2382e7ffd3
kornicameister <kornicameister@gmail.com>
2012-10-21 19:09:02 +0200

    #22 added store to hold measure's data for Inventory grid

--- WebContent/page/app/store/Measures.js
+++ WebContent/page/app/store/Measures.js
@@ -4,3 +4,16 @@
  * Date   : 19.10.12
  * Time   : 10:37
  */
+
+/**
+ * @class WMS.store.Measures
+ * @description This class represent store unit configured
+ * to operate directly on Measure
+ */
+Ext.define('WMS.store.Measures', {
+    extend: 'Ext.data.Store',
+    model : 'WMS.model.entity.Measure',
+
+    autoLoad: true,
+    autoSync: false
+});
