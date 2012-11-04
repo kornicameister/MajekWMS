37ec5dd29f521fee954aab3d75aaee911e62adae
kornicameister <kornicameister@gmail.com>
2012-11-04 11:42:09 +0100

    #36 pushed it to external class, which in fact defines only title and all the rest is hidden in abstract class that accepts data format as defined in Client model
    Signed-off-by: kornicameister <kornicameister@gmail.com>

---
+++ WebContent/page/app/view/manager/recipient/Details.js
@@ -0,0 +1,13 @@
+/*
+ * Project: MajekWMS
+ * User   : kornicameister
+ * Date   : 02.11.12
+ * Time   : 15:15
+ */
+
+Ext.define('WMS.view.manager.recipient.Details', {
+    extend            : 'WMS.panel.details.Client',
+    alternateClassName: 'WMS.panel.details.Recipient',
+    alias             : 'widget.recipientdetails',
+    title             : 'Szczegóły odbiorcy'
+});
