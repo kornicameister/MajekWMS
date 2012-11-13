051e3a90f88de5f7787b37e90446e4c82b086d6a
kornicameister <kornicameister@gmail.com>
2012-11-14 00:52:37 +0100

    #9 big refractoring due to changing IDE
    Signed-off-by: kornicameister <kornicameister@gmail.com>

--- src/wms/servlets/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * In this package all servlets can be found.
- * Those considered as more complex resides in subpackages
- * more general can be found in root folder for this package 
- * @author kornicameister
- *
- */
-package wms.servlets;
