ce3c7e499be7ded40d18ce2c9e75d739ce6b5db7
kornicameister <kornicameister@gmail.com>
2013-01-11 20:27:03 +0100

    #51 -> some stuff at chapter 3 about warehouses

---
+++ diploma/compile.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+TARGET=$1;
+COMPILE_MODE=$2;
+
+echo "Compiling file $TARGET in mode $COMPILE_MODE"
+
+echo "Compile One";
+pdflatex -output-director build $TARGET.tex >> /dev/null;
+
+if [ "$COMPILE_MODE" = "bib" ]
+then
+	cd build
+		echo "Bibliography";
+		bibtex $TARGET.aux
+	cd ../
+
+	echo "Compile Three";
+	pdflatex -output-director build $TARGET.tex >> /dev/null
+
+	echo "Compile Four";
+	pdflatex -output-director build $TARGET.tex >> /dev/null
+fi
