f8995f4468f712a2fcd9d68be92113f5c2875fe5
kornicameister <kornicameister@gmail.com>
2013-02-06 23:49:51 +0100

    #55 -> diploma
    Provided minted package that allows to build some
    impressive code listings :)
    
    Signed-off-by: kornicameister <kornicameister@gmail.com>

--- diploma/compile.sh
+++ diploma/compile.sh
@@ -6,7 +6,7 @@ COMPILE_MODE=$2;
 echo "Compiling file $TARGET in mode $COMPILE_MODE"
 
 echo "Compile One";
-pdflatex -output-director build $TARGET.tex >> /dev/null;
+pdflatex -shell-escape -output-director build $TARGET.tex >> /dev/null;
 
 if [ "$COMPILE_MODE" = "bib" ]
 then
@@ -16,8 +16,8 @@ then
 	cd ../
 
 	echo "Compile Three";
-	pdflatex -output-director build $TARGET.tex >> /dev/null
+	pdflatex -shell-escape -output-director build $TARGET.tex >> /dev/null
 
 	echo "Compile Four";
-	pdflatex -output-director build $TARGET.tex >> /dev/null
+	pdflatex -shell-escape -output-director build $TARGET.tex >> /dev/null
 fi
